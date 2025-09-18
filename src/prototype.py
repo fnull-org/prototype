9444829dadb9ad487d45b8c75a6c757b51baafcb
shouko.- <144153287+skotish06@users.noreply.github.com>
2025-09-18 23:33:40 +0700

    rearrange file!

---
+++ src/prototype.py
@@ -0,0 +1,5 @@
+from utils import get_local_ip
+
+if __name__ == "__main__":
+    ip_address = get_local_ip()
+    print(ip_address)
---
+++ src/utils.py
@@ -0,0 +1,14 @@
+import socket
+
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(("8.8.8.8", 80))
+        local_ip = s.getsockname()[0]
+    except Exception:
+        local_ip = "127.0.0.1"
+    finally:
+        s.close()
+    return local_ip
+
+    
